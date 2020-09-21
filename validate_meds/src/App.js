1de8b6a94d8350a12a627db1a9c765f9ffd98745
craig-lion <craig.m.lindahl@gmail.com>
2020-09-21 12:18:48 -0700

    switched to formik form

--- validate_meds/src/App.js
+++ validate_meds/src/App.js
@@ -1,23 +1,69 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
+import { useFormik } from 'formik';
+
 import './App.css';
 
 const App = () => {
+  const [oneMed, setOneMed] = useState({
+    name: '',
+    rxnorm: null,
+  })
+  const [allMeds, setAllMeds] = useState([])
+  const [count, setCount] = useState(1)
   const newMedication = () => {
     console.log('ye dis click fam')
+    setCount(count + 1)
+  }
+  const saveMedications = () => {
+    console.log('medications saved')
   }
 
   const handleChange = (e) => {
     console.log(e.target.value)
+    setOneMed({...oneMed, name:e.target.value})
   }
+
+  const formik = useFormik({
+    initialValues: {
+      drugName: '',
+    },
+    onSubmit: name => {
+      setOneMed({...oneMed, name})
+    },
+  });
+
+  const OneInput = (c) => (
+      <NewMedContainer key={c}>
+        <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="drugName">Medication Name</label>
+      <input
+        name="drugName"
+        onChange={formik.handleChange}
+        value={formik.values.drugName}
+        size="40"
+      />
+      <button type="submit">Submit</button>
+      <button type="button" onClick={newMedication}>Enter Another Medication</button>
+     </form>
+       </NewMedContainer>
+  )
+
+
+  const makeAllInputs = () => {
+    let array = []
+    for (let i=0; i < count; i++) {
+      array.push(OneInput(count))
+    }
+    return array
+  }
+
+  const allInputs = makeAllInputs()
+
   return (
       <>
-        <NewMedContainer>
-          <p> Enter Medication Here </p>
-          <input size="50" onChange={handleChange}></input>
-          <button type="button" onClick={newMedication}>Enter Another Medication</button>
-        </NewMedContainer>
-        <button type="button" >Save All Medications</button>
+        {allInputs}
+        <button type="button" onClick={saveMedications}>Save All Medications</button>
       </>
   );
 }
