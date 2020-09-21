ce901efc215190f9a5197e53388471720e68c8de
craig-lion <craig.m.lindahl@gmail.com>
2020-09-21 10:16:00 -0700

    added button to save all medications, added onChange to capture form input

--- validate_meds/src/App.js
+++ validate_meds/src/App.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import './App.css';
 
 const App = () => {
   const newMedication = () => {
     console.log('ye dis click fam')
   }
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+  }
   return (
       <>
-        <p> Enter Medication Here </p>
-        <input size="50"></input>
-        <button type="button" onClick={newMedication}>Enter Another Medication</button>
+        <NewMedContainer>
+          <p> Enter Medication Here </p>
+          <input size="50" onChange={handleChange}></input>
+          <button type="button" onClick={newMedication}>Enter Another Medication</button>
+        </NewMedContainer>
+        <button type="button" >Save All Medications</button>
       </>
   );
 }
+
+const NewMedContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 
 export default App;
